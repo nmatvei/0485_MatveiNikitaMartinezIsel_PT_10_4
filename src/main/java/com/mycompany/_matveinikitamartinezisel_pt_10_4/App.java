d03da26cd78cd2c40a65fb3d876f99c871253ce7
nikit <nikit@MateBook_Nikita>
2025-03-11 13:24:07 +0100

    Commit incial

---
+++ src/main/java/com/mycompany/_matveinikitamartinezisel_pt_10_4/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._matveinikitamartinezisel_pt_10_4;
+
+/**
+ *
+ * @author nikit
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
